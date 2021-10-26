990b34f3e253591cd9c95c3064d615fb64dac60d
Bhavana-Pisupati <bhavs3012@gmail.com>
2021-10-26 15:54:27 +0530

    Find Region

--- hw6_social.py
+++ hw6_social.py
@@ -107,7 +107,8 @@ Parameters: dataframe ; str
 Returns: str
 '''
 def getRegionFromState(stateDf, state):
-    return
+    row=stateDf.loc[stateDf['state'] == state, 'region']
+    return row.values[0]
 
 
 '''
@@ -302,8 +303,8 @@ if __name__ == "__main__":
     # test.testParseName()
     # test.testParsePosition()
     # test.testParseState()
-    test.testFindHashtags()
-
+    # test.testFindHashtags()
+    test.testGetRegionFromState()
     ## Uncomment these for Week 2 ##
     """print("\n" + "#"*15 + " WEEK 2 TESTS " +  "#" * 16 + "\n")
     test.week2Tests()
